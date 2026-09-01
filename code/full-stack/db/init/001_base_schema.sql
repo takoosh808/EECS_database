e62ac17fd1d3d750c517d92569219dee4fc86e65
NastyChopSticks <kaden.metzger@gmail.com>
2026-09-01 09:04:42 -0700

    pushing db changes

--- code/full-stack/db/init/001_base_schema.sql
+++ code/full-stack/db/init/001_base_schema.sql
@@ -11,11 +11,7 @@ CREATE TABLE IF NOT EXISTS users(
   CONSTRAINT users_role_chk CHECK (role IN ('user', 'admin'))
 );
 
-CREATE TABLE IF NOT EXISTS labs (
-  lab_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name TEXT NOT NULL UNIQUE,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
+
 
 CREATE TABLE IF NOT EXISTS categories (
   category_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -24,54 +20,48 @@ CREATE TABLE IF NOT EXISTS categories (
 );
 
 CREATE TABLE IF NOT EXISTS assets (
-  asset_id UUID DEFAULT gen_random_uuid(),
+  asset_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL,
-  category_id UUID NOT NULL REFERENCES categories(category_id) ON DELETE RESTRICT,
-  lab_id UUID NOT NULL REFERENCES labs(id) ON DELETE RESTRICT,
-  --category_id TEXT NOT NULL,
-  --lab_id TEXT NOT NULL,
   description TEXT,
   image_url TEXT,
   location TEXT,
   serial_number TEXT NOT NULL UNIQUE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS asset_categories (
+  asset_id UUID NOT NULL REFERENCES assets(asset_id) ON DELETE CASCADE,
+  category_id UUID NOT NULL REFERENCES categories(category_id) ON DELETE CASCADE,
+
   PRIMARY KEY (asset_id, category_id)
 );
 
 CREATE TABLE IF NOT EXISTS asset_checkout(
   checkout_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   asset_id UUID NOT NULL REFERENCES assets(asset_id) ON DELETE CASCADE,
-  category_id UUID NOT NULL REFERENCES assets(category_id) ON DELETE CASCADE,
-  user_id UUID NOT NULL,
+  user_id UUID NOT NULL REFERENCES user(user_id),
+  r4r TEXT,
   checkout_status VARCHAR(30) DEFAULT 'PENDING',
   request_date TIMESTAMPTZ DEFAULT NOW(),
   checkout_length INTEGER,
+  due_date TIMESTAMPTZ,
   processed_by UUID,
   returned_at TIMESTAMPTZ,
   CHECK (checkout_status IN ('PENDING','ACTIVE', 'RETURN_REQUESTED', 'RETURN_APPROVED', 'RETURNED', 'DENIED'))
 );
 
 
-CREATE TYPE message_type_enum AS ENUM (
-    'INSTRUCTION',
-    'REASON',
-    'NOTIFICATION'
-);
 
 --sender_id needs to reference admin id once the table exists for users
 CREATE TABLE IF NOT EXISTS asset_checkout_messages(
-  checkout_message_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  checkout_id UUID NOT NULL REFERENCES asset_checkout(id) ON DELETE CASCADE,
-  sender_id UUID DEFAULT NULL,
-  message_type message_type_enum NOT NULL,
+  message_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  sender_id UUID NOT NULL REFERENCES user(user_id),
+  receiver_id UUID NOT NULL REFERENCES user(user_id),
   message_text TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX one_active_checkout_per_asset
-ON asset_checkout(asset_id)
-WHERE checkout_status = 'ACTIVE' AND returned_at IS NULL;
 
 CREATE INDEX IF NOT EXISTS idx_assets_category_id ON assets(category_id);
 CREATE INDEX IF NOT EXISTS idx_assets_lab_id ON assets(lab_id);
