274b28899726d85b25c2b49de44ecd513f147ca1
NastyChopSticks <kaden.metzger@gmail.com>
2026-09-08 15:31:02 -0700

    docker by default

--- code/full-stack/db/init/db_index.ts
+++ code/full-stack/db/init/db_index.ts
@@ -14,27 +14,27 @@ interface DBConfig {
 }
 
 //for docker
-// const config: DBConfig = {
-//   host: process.env.DB_HOST || process.env.POSTGRES_HOST || "localhost",
-//   port: Number(process.env.DB_PORT || process.env.POSTGRES_PORT) || 5432,
-//   database: process.env.DB_NAME || process.env.POSTGRES_DB || "postgres",
-//   user: process.env.DB_USER || process.env.POSTGRES_USER || "postgres",
-//   password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD || "",
-// };
-
-// const pool = process.env.DATABASE_URL
-//   ? new Pool({ connectionString: process.env.DATABASE_URL })
-//   : new Pool(config);
-
-//for local db and using npm run dev
 const config: DBConfig = {
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || "postgres",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "",
+  host: process.env.DB_HOST || process.env.POSTGRES_HOST || "localhost",
+  port: Number(process.env.DB_PORT || process.env.POSTGRES_PORT) || 5432,
+  database: process.env.DB_NAME || process.env.POSTGRES_DB || "postgres",
+  user: process.env.DB_USER || process.env.POSTGRES_USER || "postgres",
+  password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD || "",
 };
 
-const pool = new Pool(config);
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : new Pool(config);
+
+//for local db and using npm run dev
+// const config: DBConfig = {
+//   host: process.env.DB_HOST || "localhost",
+//   port: Number(process.env.DB_PORT) || 5432,
+//   database: process.env.DB_NAME || "postgres",
+//   user: process.env.DB_USER || "postgres",
+//   password: process.env.DB_PASSWORD || "",
+// };
+
+//const pool = new Pool(config);
 
 export default pool;
