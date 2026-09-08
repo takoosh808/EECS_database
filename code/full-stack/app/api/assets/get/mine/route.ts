e8d546ffbfdfdbe548983ec27977008f72278206
NastyChopSticks <kaden.metzger@gmail.com>
2026-09-08 16:59:09 -0700

    finished integration, project almost complete?

--- code/full-stack/app/api/assets/get/mine/route.ts
+++ code/full-stack/app/api/assets/get/mine/route.ts
@@ -2,12 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import pool from "../../../../../db/init/db_index";
 
 export async function GET(req: NextRequest) {
+  const cookies = req.cookies;
+  const userId = cookies.get("auth_user")?.value;
+  console.log("test");
+  console.log(userId);
   try {
     const result = await pool.query(
       `
-        SELECT asset_id, name AS asset, serial_number, description, image_url, location, created_at, updated_at
-        FROM assets
+       SELECT ac.checkout_id, a.name AS asset, ac.checkout_length, ac.request_date, ac.due_date
+        FROM asset_checkout ac JOIN assets a ON ac.asset_id = a.asset_id
+        WHERE ac.checkout_status = 'ACTIVE' AND ac.user_id = $1
         `,
+      [userId],
     );
     return NextResponse.json(result.rows);
   } catch (err) {
